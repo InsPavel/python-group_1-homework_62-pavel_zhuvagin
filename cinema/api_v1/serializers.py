7eddaa6d2a27aa662bd5a7872d4a29d3e694152a
pavel <zhpv07@gmail.com>
2019-03-04 19:50:34 -0500

    Добавлены ссылки и id на объекты в API (просмотр из списка по id).

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -3,30 +3,40 @@ from rest_framework import serializers
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:movie-detail')
+
     class Meta:
         model = Movie
-        fields = ('id', 'name', 'categories', 'description', 'poster', 'release_date', 'finish_date', 'is_deleted')
+        fields = ('url', 'id', 'name', 'categories', 'description', 'poster', 'release_date', 'finish_date', 'is_deleted')
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:category-detail')
+
     class Meta:
         model = Category
-        fields = ('name', 'description', 'is_deleted')
+        fields = ('url', 'id', 'name', 'description', 'is_deleted')
 
 
 class HallSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:hall-detail')
+
     class Meta:
         model = Hall
-        fields = ('name', 'is_deleted')
+        fields = ('url', 'id', 'name', 'is_deleted')
 
 
 class SeatSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:seat-detail')
+
     class Meta:
         model = Seat
-        fields = ('hall', 'row', 'place', 'is_deleted')
+        fields = ('url', 'id', 'hall', 'row', 'place', 'is_deleted')
 
 
 class ShowSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:show-detail')
+
     class Meta:
         model = Show
-        fields = ('movie', 'hall', 'start_of_show', 'finish_of_show', 'price', 'is_deleted')
+        fields = ('url', 'id', 'movie', 'hall', 'start_of_show', 'finish_of_show', 'price', 'is_deleted')
