b13616d29fbccf7231b132fd74190fd50a09fe16
pavel <zhpv07@gmail.com>
2019-03-13 23:37:16 -0400

    Выведенно название зала. Показано сообщение если сеансов нет.

--- frontend/src/containers/HallDetail/HallDetail.js
+++ frontend/src/containers/HallDetail/HallDetail.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {SHOWS_URL} from "../../api-urls";
+import {HALLS_URL, SHOWS_URL} from "../../api-urls";
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 import HallDeleteButton from "../../componenets/HallDeleteButton/HallDeleteButton";
@@ -7,7 +7,8 @@ import HallDeleteButton from "../../componenets/HallDeleteButton/HallDeleteButto
 class HallDetail extends Component {
     state = {
         shows: [],
-        show: []
+        show: [],
+        hall: []
     };
 
     componentDidMount() {
@@ -34,6 +35,19 @@ class HallDetail extends Component {
                 })
             })
             .catch(error => console.log(error));
+
+        axios.get(HALLS_URL + match.params.id)
+            .then(response => {
+                console.log(response.data);
+                return response.data
+            })
+            .then(hall => {
+                this.setState((prevState => {
+                    let newState = {...prevState};
+                    newState.hall = hall;
+                    return newState;
+                }))
+            })
     }
 
     formatDate = (date) => {
@@ -47,19 +61,24 @@ class HallDetail extends Component {
     render(){
         const match = this.props.match;
         return  <div>
-            <h3>Сеансы на три дня:</h3>
-            {this.state.show.map(show => (
-                <p key={show.id}>
-                    Фильм: {show.movie.name}
-                    Дата: {this.formatDate(show.start_of_show)}.
-                    Время сеанса: {this.formatTime(show.start_of_show)} - {this.formatTime(show.finish_of_show)}.
-                    Стоимость билета: {show.price} сом
-                </p>
-            ))}
-            <NavLink to={'/halls/' + match.params.id + '/edit'} className='btn btn-primary mr-2'>Edit</NavLink>
-            <span  className="btn btn-primary mr-2"><HallDeleteButton id={match.params.id}/></span>
-            <NavLink to={'/'} className='btn btn-primary'>Movies</NavLink>
-        </div>
+            <h3>Зал: {this.state.hall.name}</h3>
+            <h5>Репертуар сеансов на три дня:</h5>
+            {this.state.show.length > 0 ? <span>
+                {this.state.show.map(show => (
+                    <p key={show.id}>
+                        Фильм: {show.movie.name}.
+                        Дата: {this.formatDate(show.start_of_show)}.
+                        Время сеанса: {this.formatTime(show.start_of_show)} - {this.formatTime(show.finish_of_show)}.
+                        Стоимость билета: {show.price} сом
+                    </p>
+                ))}
+                <NavLink to={'/halls/' + match.params.id + '/edit'} className='btn btn-primary mr-2'>Edit</NavLink>
+                </span> : <p>На данный момент сеансов в этом зале нет</p>
+            }
+                <span  className="btn btn-primary mr-2"><HallDeleteButton id={match.params.id}/></span>
+                <NavLink to={'/'} className='btn btn-primary mr-2'>Movies</NavLink>
+                <NavLink to={'/halls/'} className='btn btn-primary'>Halls</NavLink>
+            </div>
     }
 }
 
