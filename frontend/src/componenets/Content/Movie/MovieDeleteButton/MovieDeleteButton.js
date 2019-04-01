e70449d4df1155824ffc2a83d923608bf8897934
pavel <zhpv07@gmail.com>
2019-04-01 10:08:41 -0400

    Модифицированно удаление фильма.

--- frontend/src/componenets/Content/Movie/MovieDeleteButton/MovieDeleteButton.js
+++ frontend/src/componenets/Content/Movie/MovieDeleteButton/MovieDeleteButton.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import axios from "axios";
 import {MOVIES_URL} from "../../../../api-urls";
+import connect from "react-redux/es/connect/connect";
+
 
 class MovieDeleteButton extends Component {
     deleteMovie = () => {
+        const token = this.props.token;
         axios.delete(MOVIES_URL + this.props.id + '/', {
             data: { foo: "bar" },
-            headers: { 'Authorization': 'Token ' + localStorage.getItem('auth-token')}
+            headers: { 'Authorization': 'Token ' + token}
             })
             .then(movie => window.history.back())
     };
@@ -15,5 +18,8 @@ class MovieDeleteButton extends Component {
         return <div onClick={this.deleteMovie}>Delete</div>
     }
 }
+const mapStateToProps = state => state.auth;
+const mapDispatchToProps = dispatch => ({});
 
-export default MovieDeleteButton;
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDeleteButton);
+
