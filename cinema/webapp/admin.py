28ae560c87be23e7a42a61719cad065efdebdbd1
pavel <zhpv07@gmail.com>
2019-04-03 19:34:22 -0400

    Добавлена модель для токенов регистрации.

--- cinema/webapp/admin.py
+++ cinema/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import Movie, Category, Hall, Seat, Show, Ticket, Discount, Book
+from webapp.models import Movie, Category, Hall, Seat, Show, Ticket, Discount, Book, RegistrationToken
 
 
 class MovieAdmin(admin.ModelAdmin):
@@ -14,6 +14,11 @@ def list_admin_with_pk(*fields):
     return PkListAdmin
 
 
+class RegistrationTokenAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'created_at']
+    readonly_fields = ['token']
+
+
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Category, list_admin_with_pk('name'))
 admin.site.register(Hall,  list_admin_with_pk('name'))
@@ -22,3 +27,5 @@ admin.site.register(Show, list_admin_with_pk('movie', 'hall', 'start_of_show', '
 admin.site.register(Ticket, list_admin_with_pk('show', 'seat', 'discount'))
 admin.site.register(Discount, list_admin_with_pk('name', 'discount', 'start_date', 'finish_date'))
 admin.site.register(Book, list_admin_with_pk('code', 'show', 'get_seats_display', 'status', 'created_at', 'updated_at'))
+admin.site.register(RegistrationToken, RegistrationTokenAdmin)
+
--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 import random, string
 from django.conf import settings
+import uuid
+
+
+class RegistrationToken(models.Model):
+    token = models.UUIDField(default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def is_expired(self):
+        delta = now() - self.created_at
+        delta_hours = delta.total_seconds() / 3600
+        return delta_hours > settings.TOKEN_EXPIRATION_HOURS
+
+    def __str__(self):
+        return "%s" % self.token
 
 
 class SoftDeleteManager(models.Manager):
